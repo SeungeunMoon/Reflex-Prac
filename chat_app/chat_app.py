ec807fd81526b1197f9146dee97a07d40d248105
estie <harin0375@gmail.com>
2025-03-17 23:23:21 +0900

    ai chat 초안

---
+++ chat_app/chat_app.py
@@ -0,0 +1,46 @@
+import reflex as rx
+from .page1 import page_1
+
+class State(rx.State):
+    q: str
+    qa_list: list[list[str]] = \
+        [
+            ["1+1=", "11입니다"],
+            ["3.9와 3.11 중에 어느게 크지?", "3.11입니다."]
+        ]
+    
+    def append_qa(self, d):
+        self.q = d["q"]
+        self.qa_list.append(
+            [self.q, self.ai_answer(self.q)]
+        )
+    def ai_answer(self, q):
+        return "답변을 거부함."
+
+def qa(qna):
+    return rx.box(
+        rx.box(qna[0],
+               background_color = "green",
+               text_align="right"),
+        rx.box(qna[1],
+               background_color="orange",
+               text_align="left")
+    )
+
+
+@rx.page()
+def index() -> rx.Component:
+    return rx.container(
+        rx.foreach(State.qa_list, qa),
+        rx.form(
+            rx.input(
+                placeholder="write a question",
+                name="q"
+            ),
+            on_submit=lambda e: State.append_qa(e),
+            #State.append_qa가 사용자입력 데이터를 {"q": 사용자입력} 사전으로 받아서 처리하게 됩니다.
+            reset_on_submit=True
+        )
+    )
+
+app = rx.App()
--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,5 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="ToDoList",
+    app_name="chat_app",
 )
