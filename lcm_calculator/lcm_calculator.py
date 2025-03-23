4a28323ddad660b69c1c721930be2bddb21350a9
estie <harin0375@gmail.com>
2025-03-23 15:06:15 +0900

    update answer format

--- lcm_calculator/lcm_calculator.py
+++ lcm_calculator/lcm_calculator.py
@@ -2,22 +2,22 @@ import reflex as rx
 import math
 
 class State(rx.State):
-    answer: int
+    answer: str
     
     @rx.event
     def calculator(self, x):
         nums = x["n"]
         
         cal = math.lcm(*[int(i) for i in nums.strip().split(" ")])
-        self.answer = cal
+        self.answer = f"{cal:,}"
 
-@rx.page()
+@rx.page(on_load=rx.set_focus('n'))
 def index():
     return rx.vstack(
         rx.heading("최소공배수 계산기"),
         rx.text("띄어쓰기로 구분하여 두 개 이상의 자연수를 입력해주세요."),
         rx.form(
-            rx.input(placeholder="입력 후 엔터", name='n'),
+            rx.input(placeholder="입력 후 엔터", name='n',id='n'),
             on_submit= State.calculator,
             reset_on_submit=True,
         ),
